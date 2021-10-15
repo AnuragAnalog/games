0956321d3d2422233344001a0c6537959a72ee74
Subhash Sarangi <subhashsarangi123@gmail.com>
2021-10-15 12:59:00 +0530

    Refined rock-paper-scissors code

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,27 +1,40 @@
 import random
+
+wins = {
+	('r', 's'),
+	('s', 'p'),
+	('p', 'r')
+}
+
+USER = "user"
+COMPUTER = "computer"
+TIE = "tie"
+
 def play():
-    choice=input('Enter a choice..."r" for Rock,"p" for Paper,"s" for Scissors')
+    choice=input('Enter a choice..."r" for Rock,"p" for Paper,"s" for Scissors: ')
     user_choice=choice.lower()
     computer_choice=random.choice(['r','p','s'])
+
     winner=None
     if(user_choice==computer_choice):
-        print('You are tied with the computer...')
-    elif(user_choice=='r' and computer_choice=='p'):
-        winner='So, the user chooses {} and the computer chooses {}, SO, Computer wins...'.format(user_choice, computer_choice)
-    elif(user_choice=='r' and computer_choice=='s'):
-        winner='So, the user chooses {} and the computer chooses {}, SO, User wins'.format(user_choice, computer_choice)
-    elif(user_choice=='p' and computer_choice=='r'):
-        winner='So, the user chooses {} and the computer chooses {}, SO, User wins'.format(user_choice, computer_choice)
-    elif(user_choice=='p' and computer_choice=='s'):
-        winner='So, the user chooses {} and the computer chooses {}, SO, Computer wins'.format(user_choice, computer_choice)
-    elif(user_choice=='s' and computer_choice=='p'):
-        winner='So, the user chooses {} and the computer chooses {}, SO, User wins'.format(user_choice, computer_choice)
-    elif(user_choice=='s' and computer_choice=='r'):
-        winner='So, the user chooses {} and the computer chooses {}, SO, Computer wins'.format(user_choice, computer_choice)
+        winner = TIE
+    elif (user_choice, computer_choice) in wins :
+        winner = USER
+    elif (computer_choice, user_choice) in wins :
+        winner = COMPUTER
+
+    if winner :
+        if winner == TIE :
+            print('You are tied with the computer...')
+        else :
+            print(f"The user chooses {user_choice}, and the computer chooses {computer_choice}. So, the {winner} wins!")
     else:
         print('Something is wrong')
+
     return winner  
+
 def main():
-    print(play())
+    _winner = play()
+
 if __name__=='__main__':
     main()
