4458ac19892fa0ba0eee7066d39fa88fadf3b253
CambellX <91762187+CambellX@users.noreply.github.com>
2021-10-18 14:50:10 -0700

    Update jumbled_words_game.py
    Added a loop that would repeat until the correct word is entered

--- jumbled_words_game.py
+++ jumbled_words_game.py
@@ -1,5 +1,5 @@
 import random
-
+tries = 0
 words = ['rainbow', 'computer', 'science', 'programming',
              'mathematics', 'player', 'condition', 'reverse',
              'water', 'board', 'geeks']
@@ -9,7 +9,16 @@ random_word = random.sample(word, len(word))
 jumbled = ''.join(random_word)
 print(f'Here is your word: {jumbled}')
 s = input("Enter the correct answer:")
-if s == word:
-	print("Correct")
-else:
-	print("Try again next time!!")
+tries+=1
+while s!= word:
+  tries+=1
+  s = input("Incorrect.\n Try again\n")
+  
+  if s==word:
+    break
+
+print("Correct!")
+print("You took", tries, "tries" )
+
+exit()
+
