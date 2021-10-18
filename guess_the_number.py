07b2f03c1f89d0d2922793b723fde421eca609d6
Nirajan Bist <86724281+neon-bist@users.noreply.github.com>
2021-10-18 12:02:10 +0545

    Added number of tries for players attempts
    @AnuragAnalog Hope you liked it.. hacktoberfest open source

--- guess_the_number.py
+++ guess_the_number.py
@@ -5,15 +5,16 @@ import random
 def guess_the_number():
     print("I have a number, guess that\n")
     num = random.randint(1000)
-    
+    numberOfTrys=0
     while True:
         guess = int(input("Guess the number: "))
+        numberOfTrys += 1
         if guess > num:
             print("My number is smaller than yours\nTry Again")
         elif guess < num:
             print("My number is larger than yours\nTry Again")
         else:
-            print("Hooray! You have guessed it right.")
+            print(f"Hooray! You have guessed it right in {numberOfTrys} attempt(s).")
             return
     
 if __name__ == "__main__":
